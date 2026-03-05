0597a8c16baaeab25b7b750f6566094086569e18
youssef mohamed <youssefhusseinnn1@gmail.com>
2026-03-06 00:28:37 +0200

    backend
    error in db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Fix for querySrv ECONNREFUSED on some networks/Windows
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
     try {
